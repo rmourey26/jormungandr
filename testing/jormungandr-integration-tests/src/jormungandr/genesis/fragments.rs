8ec7a1a01c6cc73b149cc0bf5a8895d243ba4f5e
dkijania <dariusz.kijania@iohk.io>
2021-01-25 15:08:11 +0100

    added new test which expect failure

--- testing/jormungandr-integration-tests/src/jormungandr/genesis/fragments.rs
+++ testing/jormungandr-integration-tests/src/jormungandr/genesis/fragments.rs
@@ -209,7 +209,7 @@ pub fn test_all_adversary_fragments() {
 }
 
 #[test]
-pub fn one_hundreds_addresses() {
+pub fn test_increased_block_content_max_size() {
     let receivers: Vec<Wallet> = std::iter::from_fn(|| Some(startup::create_new_account_address()))
         .take(98)
         .collect();
@@ -221,7 +221,8 @@ pub fn one_hundreds_addresses() {
         &[stake_pool_owner.clone()],
         &[],
         &mut ConfigurationBuilder::new()
-            .with_consensus_genesis_praos_active_slot_coeff(ActiveSlotCoefficient::MAXIMUM),
+            .with_consensus_genesis_praos_active_slot_coeff(ActiveSlotCoefficient::MAXIMUM)
+            .with_block_content_max_size(8192.into()),
     )
     .unwrap();
 
@@ -239,4 +240,38 @@ pub fn one_hundreds_addresses() {
             stake_pool_owner_stake.into(),
         )
         .unwrap();
+}
+
+#[test]
+pub fn test_block_content_max_size_below_transaction_size() {
+    let receivers: Vec<Wallet> = std::iter::from_fn(|| Some(startup::create_new_account_address()))
+        .take(98)
+        .collect();
+    let mut stake_pool_owner = startup::create_new_account_address();
+
+    let stake_pool_owner_stake = 1;
+
+    let (jormungandr, _stake_pools) = startup::start_stake_pool(
+        &[stake_pool_owner.clone()],
+        &[],
+        &mut ConfigurationBuilder::new()
+            .with_consensus_genesis_praos_active_slot_coeff(ActiveSlotCoefficient::MAXIMUM)
+            .with_block_content_max_size(8092.into()),
+    )
+    .unwrap();
+
+    let transaction_sender = FragmentSender::new(
+        jormungandr.genesis_block_hash(),
+        jormungandr.fees(),
+        FragmentSenderSetup::should_stop_at_error(),
+    );
+
+    assert!(transaction_sender
+        .send_transaction_to_many(
+            &mut stake_pool_owner,
+            &receivers,
+            &jormungandr,
+            stake_pool_owner_stake.into(),
+        )
+        .is_err());
 }
